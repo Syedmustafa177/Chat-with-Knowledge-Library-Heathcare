2f23fedc10a3d96b581242d41e6489ef4e606c9c
syed mustafa <113262233+Syedmustafa177@users.noreply.github.com>
2024-06-29 23:51:16 +0530

    added gemini-1.5-flash new gimi model

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def get_conversational_chain():
     Answer:
     """
 
-    model = ChatGoogleGenerativeAI(model="gemini-pro",
+    model = ChatGoogleGenerativeAI(model="gemini-1.5-flash",
                              temperature=0.3)
 
     prompt = PromptTemplate(template = prompt_template, input_variables = ["context", "question"])
